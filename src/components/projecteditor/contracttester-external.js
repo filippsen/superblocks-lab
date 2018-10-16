eedb9c2cfb97a116dc447416f4c9cb2f92bf9a40
Maicon Diniz Filippsen <filippsen@blockie.org>
2018-10-16 21:55:52 +0200

    Update test reference to re-deploy target contract during beforeEach stage
    + Add contractInstance data to be accessed by tests
    
    + Add beforeEach function to test for auto-redeployment
    
    + Add repeated test to ensure beforeEach behavior is working as expected
    
    * Change instance test access from Lab-deployed contract to contract
    deployed in beforeEach stage

--- src/components/projecteditor/contracttester-external.js
+++ src/components/projecteditor/contracttester-external.js
@@ -53,49 +53,30 @@ export function action_test(web3, instance, contract_address, account_address, a
     const utilityLibrary = new ContractTesterLib();
 
     describe('User test action: manually check contract data', function (done) {
-        // NOTE: the following tests are intended
-        //       to target the Hello World template
-        it('matches message data', function (done) {
-            var expectedValue = "Hello World!";
-            instance.message(function(error, result) {
-                if(error) {
-                    console.error(error);
-                    done(error);
-                } else {
-                    if(result !== expectedValue) {
-                        done(new Error(result));
-                    } else {
-                        done();
-                    }
-                }
-            });
-        });
+        var contractInstance;
 
-        // TODO: FIXME: mutates data
-        //              check assumption within the same context
-        //              needs a reset before or afterwards
-        //
-        it('update message data', function (done) {
-            const gas_price="0x3B9ACA00";
-            const gas_limit="0x3b8260";
+        beforeEach(function (done) {
+            // TODO: FIXME: access to compiler or pre-built data
+            const contractBin = "0x6060604052341561000f57600080fd5b6040516103c13803806103c1833981016040528080518201919050508060009080519060200190610041929190610048565b50506100ed565b828054600181600116156101000203166002900490600052602060002090601f016020900481019282601f1061008957805160ff19168380011785556100b7565b828001600101855582156100b7579182015b828111156100b657825182559160200191906001019061009b565b5b5090506100c491906100c8565b5090565b6100ea91905b808211156100e65760008160009055506001016100ce565b5090565b90565b6102c5806100fc6000396000f30060606040526004361061004c576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff1680633d7403a314610051578063e21f37ce146100ae575b600080fd5b341561005c57600080fd5b6100ac600480803590602001908201803590602001908080601f0160208091040260200160405190810160405280939291908181526020018383808284378201915050505050509190505061013c565b005b34156100b957600080fd5b6100c1610156565b6040518080602001828103825283818151815260200191508051906020019080838360005b838110156101015780820151818401526020810190506100e6565b50505050905090810190601f16801561012e5780820380516001836020036101000a031916815260200191505b509250505060405180910390f35b80600090805190602001906101529291906101f4565b5050565b60008054600181600116156101000203166002900480601f0160208091040260200160405190810160405280929190818152602001828054600181600116156101000203166002900480156101ec5780601f106101c1576101008083540402835291602001916101ec565b820191906000526020600020905b8154815290600101906020018083116101cf57829003601f168201915b505050505081565b828054600181600116156101000203166002900490600052602060002090601f016020900481019282601f1061023557805160ff1916838001178555610263565b82800160010185558215610263579182015b82811115610262578251825591602001919060010190610247565b5b5090506102709190610274565b5090565b61029691905b8082111561029257600081600090555060010161027a565b5090565b905600a165627a7a72305820bb261bb5858618e117ce6751ee971eccf221b3efade6c29d04b739a5e37335ab00290000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000c48656c6c6f20576f726c64210000000000000000000000000000000000000000";
+            // TODO: FIXME: access to compiler or pre-built data
+            const contractABI = instance.abi;
+
             var account_nonce=0;
-
-            // TODO: FIXME: consider providing a helper function for the following code ?
             web3.eth.getTransactionCount(account_address, function(error, result) {
                 if(error) {
                     done(new Error("Could not get nonce for address " + account_address));
                 } else {
                     account_nonce=result;
-                    // TODO: FIXME: offer as library call
-                    var data = ABI.ABI.simpleEncode("update(string)", "Super Hello World!");
+
+                    const gas_price="0x3B9ACA00";
+                    const gas_limit="0x3b8260";
                     const tx=new Tx.Tx({
                         from: account_address,
-                        to: contract_address,
                         value: "0x0",
                         nonce: account_nonce,
                         gasPrice: gas_price,
                         gasLimit: gas_limit,
-                        data: data,
+                        data: contractBin,
                     });
                     tx.sign(Tx.Buffer.Buffer.from(account_key, "hex"));
 
@@ -105,19 +86,40 @@ export function action_test(web3, instance, contract_address, account_address, a
                                 console.error(error);
                                 done(error);
                             } else {
-                                done();
+                                var currentContractTransactionHash = result;
+
+                                function getTransactionReceipt(hash, cb) {
+                                    web3.eth.getTransactionReceipt(hash,(err,res)=>{
+                                        if(err || !res || !res.blockHash) {
+                                            setTimeout(()=>{getTransactionReceipt(hash, cb)},100);
+                                        }
+                                        else  {
+                                            cb(null, res);
+                                        }
+                                    });
+                                }
+
+                                getTransactionReceipt(currentContractTransactionHash, function(err, res) {
+                                    if(err) {
+                                        console.error(err);
+                                        done(err);
+                                    } else {
+                                        var contract = web3.eth.contract(contractABI);
+                                        contractInstance = contract.at(res.contractAddress);
+                                        done();
+                                    }
+                                });
                             }
-                        }
-                    );
+                    });
                 }
             });
         });
 
-        // TODO: FIXME: relies on previously mutated data
-        it('matches updated message data', function (done) {
-            // NOTE: this is a test for Hello World template
-            var expectedValue = "Super Hello World!";
-            instance.message(function(error, result) {
+        // NOTE: the following tests are intended
+        //       to target the Hello World template
+        it('matches message data', function (done) {
+            var expectedValue = "Hello World!";
+            contractInstance.message(function(error, result) {
                 if(error) {
                     console.error(error);
                     done(error);
@@ -130,18 +132,80 @@ export function action_test(web3, instance, contract_address, account_address, a
                 }
             });
         });
-    });
 
-    // TODO: FIXME: relies on previously mutated data
-    describe('User test action: check contract data via assert_call', function (done) {
-        // NOTE: this is a test for Hello World template
-        // TODO: FIXME: simplify assert_call to minimize number of parameters 
-        //              (consider implicit instance and address ?)
+        // NOTE: the following tests are intended
+        //       to target the Hello World template
+        it('update message data', function (done) {
+            const gas_price="0x3B9ACA00";
+            const gas_limit="0x3b8260";
+            var account_nonce=0;
 
-        var method="message";
-        var args=[];
-        var expectedType=["string"];
-        var expectedValue=["Super Hello World!"];
-        utilityLibrary.assert_call(instance, contract_address, method, args, expectedType, expectedValue, done);
+            web3.eth.getTransactionCount(account_address, function(error, result) {
+                if(error) {
+                    done(new Error("Could not get nonce for address " + account_address));
+                } else {
+                    account_nonce=result;
+                    var data = ABI.ABI.simpleEncode("update(string)", "Super Hello World!");
+                    const tx=new Tx.Tx({
+                        from: account_address,
+                          to: contractInstance.address,
+                          value: "0x0",
+                          nonce: account_nonce,
+                          gasPrice: gas_price,
+                          gasLimit: gas_limit,
+                          data: data,
+                    });
+                    tx.sign(Tx.Buffer.Buffer.from(account_key, "hex"));
+
+                    web3.eth.sendRawTransaction("0x"+tx.serialize().toString("hex"),
+                        function(error, result) {
+                            if(error) {
+                                console.error(error);
+                                done(error);
+                            } else {
+                                contractInstance.message(function(error, result) {
+                                    var expectedValue = "Super Hello World!";
+
+                                    if(error) {
+                                        console.error(error);
+                                        done(error);
+                                    } else {
+                                        if(result !== expectedValue) {
+                                            done(new Error(result));
+                                        } else {
+                                            // Yet another method for checking the previous assumption
+                                            var method="message";
+                                            var args=[];
+                                            var expectedType=["string"];
+                                            var expectedValue=["Super Hello World!"];
+                                            utilityLibrary.assert_call(contractInstance, contractInstance.address, method, args, expectedType, expectedValue, done);
+                                            done();
+                                        }
+                                    }
+                                });
+                            }
+                        }
+                    );
+                }
+            });
+        });
+
+        // NOTE: the following tests are intended
+        //       to target the Hello World template
+        it('repeat: matches message data', function (done) {
+            var expectedValue = "Hello World!";
+            contractInstance.message(function(error, result) {
+                if(error) {
+                    console.error(error);
+                    done(error);
+                } else {
+                    if(result !== expectedValue) {
+                        done(new Error(result));
+                    } else {
+                        done();
+                    }
+                }
+            });
+        });
     });
 }
